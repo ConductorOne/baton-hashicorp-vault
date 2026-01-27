7fff95ba28d906557f5d704bf60d84f8f2cc8fb2
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:28:59 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2

--- cmd/baton-hashicorp-vault/main.go
+++ cmd/baton-hashicorp-vault/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/conductorone/baton-hashicorp-vault/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"github.com/spf13/viper"
@@ -27,6 +28,7 @@ func main() {
 		connectorName,
 		getConnector,
 		Configurations,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
